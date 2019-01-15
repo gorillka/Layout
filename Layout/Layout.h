7863521f05301bd5d7cf3f0a2034802c54355e7f
Artem Orynko <artem.or@onixcat.com>
2019-01-15 10:41:52 +0200

    init project

---
+++ Layout/Layout.h
@@ -0,0 +1,19 @@
+//
+//  Layout.h
+//  Layout
+//
+//  Created by Gorilka on 1/15/19.
+//  Copyright © 2019 gorilka-team. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Layout.
+FOUNDATION_EXPORT double LayoutVersionNumber;
+
+//! Project version string for Layout.
+FOUNDATION_EXPORT const unsigned char LayoutVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Layout/PublicHeader.h>
+
+
